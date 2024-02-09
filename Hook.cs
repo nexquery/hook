df1a777e58fc559b8e92f59b6be04f3ca79eced6
Burak <benburakyaa@gmail.com>
2024-02-09 16:09:22 +0300

    Update Hook.cs

--- Hook.cs
+++ Hook.cs
@@ -21,44 +21,44 @@ using System.Windows.Forms;
 
 public class Hook
 {
-    public static T Get<T>(string formIsim, string isim) where T : Control
+    public static T Get<T>(string formName, string componentName) where T : Control
     {
-        Form hook = Application.OpenForms[formIsim];
+        Form hook = Application.OpenForms[formName];
         if (hook != null)
         {
-            Control nesne = hook.Controls.Find(isim, true).OfType<T>().FirstOrDefault();
-            return nesne as T;
+            Control c = hook.Controls.Find(componentName, true).OfType<T>().FirstOrDefault();
+            return c as T;
         }
         return null;
     }
 
-    public static void Action<T>(T cx, Action<T> eylem) where T : Control
+    public static void Action<T>(T cx, Action<T> act) where T : Control
     {
         if (cx != null)
         {
             if (cx.InvokeRequired)
             {
-                cx.Invoke((MethodInvoker)delegate { eylem(cx); });
+                cx.Invoke((MethodInvoker)delegate { act(cx); });
             }
             else
             {
-                eylem(cx);
+                act(cx);
             }
         }
     }
 
-    public static T[] GetArray<T>(string formIsim, params string[] isimler) where T : Control
+    public static T[] GetArray<T>(string formName, params string[] componentNames) where T : Control
     {
-        Form hook = Application.OpenForms[formIsim];
+        Form hook = Application.OpenForms[formName];
         if (hook != null)
         {
             List<T> controls = new List<T>();
-            foreach (string isim in isimler)
+            foreach (string cName in componentNames)
             {
-                Control nesne = hook.Controls.Find(isim, true).OfType<T>().FirstOrDefault();
-                if (nesne != null)
+                Control c = hook.Controls.Find(cName, true).OfType<T>().FirstOrDefault();
+                if (c != null)
                 {
-                    controls.Add(nesne as T);
+                    controls.Add(c as T);
                 }
             }
             return controls.ToArray();
@@ -80,6 +80,22 @@ public class Hook
                 {
                     action(controls);
                 }
+            }
+        }
+    }
+
+    public static void Form(string formName, Action<Form> action)
+    {
+        Form form = Application.OpenForms[formName];
+        if (form != null)
+        {
+            if (form.InvokeRequired)
+            {
+                form.Invoke((MethodInvoker)delegate { action(form); });
+            }
+            else
+            {
+                action(form);
             }
         }
     }
